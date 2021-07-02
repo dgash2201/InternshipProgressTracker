471794f2ef32638bc4ee7fb216868d7aa44ed4de
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-02 12:53:50 +0300

    feat: create dbcontext class

---
+++ InternshipProgressTracker/InternshipProgressTrackerDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace InternshipProgressTracker
+{
+    public class InternshipProgressTrackerDbContext : DbContext
+    {
+        public InternshipProgressTrackerDbContext([NotNull] DbContextOptions options) : base(options) {}
+    }
+}
