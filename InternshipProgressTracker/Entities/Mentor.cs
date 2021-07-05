714156e51094da17f460bcdf018e9339ccb923ec
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-05 12:29:43 +0300

    feat: create Mentor entity

---
+++ InternshipProgressTracker/Entities/Mentor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Entities
+{
+    public class Mentor
+    {
+        public int Id { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public int InternshipStreamId { get; set; }
+    }
+}
