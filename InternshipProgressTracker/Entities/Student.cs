f78b434c473420bf1e2fa95c38c2eabb732a148d
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-05 12:32:41 +0300

    feat: create Student entity

---
+++ InternshipProgressTracker/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int InternshipStreamId { get; set; }
+
+        public int CurrentGrade { get; set; }
+    }
+}
