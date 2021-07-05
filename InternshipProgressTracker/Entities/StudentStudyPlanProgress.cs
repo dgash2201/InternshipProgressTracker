7716127a26a890796f99325dace784eca59542ab
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-05 12:52:16 +0300

    feat: create StudentStudyPlanProgress

---
+++ InternshipProgressTracker/Entities/StudentStudyPlanProgress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Entities
+{
+    public class StudentStudyPlanProgress
+    {
+        public int StudentId { get; set; }
+
+        public int StudyPlanEntryId { get; set; }
+
+        public int Grade { get; set; }
+
+        public int GradingMentorId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime FinishTime { get; set; }
+
+        public string StudentNotes { get; set; }
+
+        public string MentorNotes { get; set; }
+    }
+}
