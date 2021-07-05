e1bee5d40a20882dee315a1465f550f1310094a1
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-05 12:46:30 +0300

    feat: create StudyPlanEntry entity

---
+++ InternshipProgressTracker/Entities/StudyPlanEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Entities
+{
+    public class StudyPlanEntry
+    {
+        public int Id { get; set; }
+
+        public int StudyPlanId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Duration { get; set; }
+    }
+}
