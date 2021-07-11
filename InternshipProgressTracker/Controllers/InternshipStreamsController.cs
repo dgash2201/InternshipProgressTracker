8cd717f1dcae432caf0a32e87a0d62376906f1f4
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-11 15:33:38 +0300

    feat: add empty InternshipStreamsController

---
+++ InternshipProgressTracker/Controllers/InternshipStreamsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class InternshipStreamsController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Create([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public Task<IActionResult> Update(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public Task<IActionResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
