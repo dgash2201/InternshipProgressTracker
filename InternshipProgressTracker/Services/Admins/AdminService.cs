e497d7d137c92013771c221ab659a95924d117ee
Danil Gashigullin <gashigullin44@gmail.com>
2021-07-25 17:15:52 +0300

    feat: create admin controller and service

---
+++ InternshipProgressTracker/Services/Admins/AdminService.cs
@@ -0,0 +1,60 @@
+﻿using InternshipProgressTracker.Database;
+using InternshipProgressTracker.Entities;
+using InternshipProgressTracker.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InternshipProgressTracker.Services.Admins
+{
+    /// <summary>
+    /// Service for admins
+    /// </summary>
+    public class AdminService : IAdminService
+    {
+        private readonly InternshipProgressTrackerDbContext _dbContext;
+        private readonly UserManager<User> _userManager;
+
+        public AdminService(InternshipProgressTrackerDbContext dbContext, UserManager<User> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Gets list of all users
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IReadOnlyCollection<User>> GetAllUsersAsync()
+        {
+            var users = await _dbContext
+                .Users
+                .ToListAsync();
+
+            return users.AsReadOnly();
+        }
+
+        /// <summary>
+        /// Sets role to user
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public async Task SetUserRoleAsync(int userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+
+            if (user == null)
+            {
+                throw new NotFoundException("User with this id was not found");
+            }
+
+            var previousRoles = await _userManager.GetRolesAsync(user);
+
+            await _userManager.RemoveFromRolesAsync(user, previousRoles);
+            await _userManager.AddToRoleAsync(user, role);
+            await _userManager.UpdateAsync(user);
+        }
+    }
+}
